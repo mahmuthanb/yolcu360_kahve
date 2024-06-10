97b8e471ac77a08a0496040bc3595f3b74dc4773
Mahmuthan <mahmuthanb@gmail.com>
2024-06-11 00:56:26 +0300

    map implemented

--- lib/feature/page/order/status/order_status.dart
+++ lib/feature/page/order/status/order_status.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:yolcu360_kahve/core/base/base_widget.dart';
 import 'package:route_map/route_map.dart';
 import 'package:yolcu360_kahve/core/res/colors.gen.dart';
@@ -17,54 +19,90 @@ class _OrderStatusPageState
     extends BaseState<OrderStatusViewModel, OrderStatusPage> {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    final Size size = MediaQuery.sizeOf(context);
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.dark.copyWith(
-          systemNavigationBarColor: AppColors.grey,
-        ),
+            statusBarColor: Colors.transparent,
+            systemNavigationBarColor: Colors.transparent),
         child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(AppDimens.l),
-            child: Column(
-              children: [
-                Row(
+          top: false,
+          bottom: false,
+          child: Stack(children: [
+            SizedBox(
+              width: size.width,
+              height: size.height,
+              child: FlutterMap(
+                mapController: MapController(),
+                options: const MapOptions(
+                  initialCenter: LatLng(41.00264914876056, 29.071838599282493),
+                  initialZoom: 14,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'com.example.app',
+                  ),
+                  MarkerLayer(
+                    rotate: true,
+                    markers: [
+                      viewModel.customMarkers[0],
+                      viewModel.customMarkers[1],
+                    ],
+                  ),
+                  PolylineLayer(
+                    polylines: [Polyline(points: viewModel.getCoordinates)],
+                  )
+                ],
+              ),
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(AppDimens.l),
+                child: Column(
                   children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.white,
-                        borderRadius: BorderRadius.circular(AppDimens.s),
-                      ),
-                      padding: const EdgeInsets.all(AppDimens.s),
-                      child: InkWell(
-                        onTap: () => Navigator.pop(context),
-                        child: const Icon(
-                          Icons.arrow_back_ios_rounded,
-                          color: AppColors.black,
-                          size: 35,
+                    Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.white,
+                            borderRadius: BorderRadius.circular(AppDimens.s),
+                          ),
+                          padding: const EdgeInsets.all(AppDimens.s),
+                          child: InkWell(
+                            onTap: () => Navigator.pop(context),
+                            child: const Icon(
+                              Icons.arrow_back_ios_rounded,
+                              color: AppColors.black,
+                              size: 35,
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    const Spacer(),
-                    Container(
-                      decoration: BoxDecoration(
-                        color: AppColors.white,
-                        borderRadius: BorderRadius.circular(AppDimens.s),
-                      ),
-                      padding: const EdgeInsets.all(AppDimens.s),
-                      child: InkWell(
-                        onTap: () {},
-                        child: const Icon(
-                          Icons.my_location,
-                          color: AppColors.black,
-                          size: 35,
+                        const Spacer(),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.white,
+                            borderRadius: BorderRadius.circular(AppDimens.s),
+                          ),
+                          padding: const EdgeInsets.all(AppDimens.s),
+                          child: InkWell(
+                            onTap: () {},
+                            child: const Icon(
+                              Icons.my_location,
+                              color: AppColors.black,
+                              size: 35,
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ],
-                )
-              ],
+                ),
+              ),
             ),
-          ),
+          ]),
         ),
       ),
     );
--- lib/feature/page/order/status/order_status_vm.dart
+++ lib/feature/page/order/status/order_status_vm.dart
@@ -1,7 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:yolcu360_kahve/core/base/base_view_model.dart';
 import 'package:injectable/injectable.dart';
+import 'package:yolcu360_kahve/core/res/assets.gen.dart';
+import 'package:yolcu360_kahve/core/util/polyline_ex.dart';
 
 @injectable
 class OrderStatusViewModel extends BaseViewModel {
   OrderStatusViewModel();
+
+  late final customMarkers = <Marker>[
+    buildHq(const LatLng(41.002663591970276, 29.07182584047572)),
+    buildPin(const LatLng(40.99851198686524, 29.061593184136033)),
+  ];
+  Marker buildPin(LatLng point) => Marker(
+        point: point,
+        width: 60,
+        height: 60,
+        child: const Icon(Icons.location_pin, size: 60, color: Colors.black),
+      );
+  Marker buildHq(LatLng point) => Marker(
+        point: point,
+        width: 50,
+        height: 50,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(9),
+            image: DecorationImage(
+              image: AppAssets.images.phYolcu360.provider(),
+            ),
+          ),
+        ),
+      );
+  List<num> unalanMetro = [40.99851198686524, 29.061593184136033];
+  List<num> yolcu360Hq = [41.002663591970276, 29.07182584047572];
+
+  final coords = encodePolyline([
+    [40.99851198686524, 29.061593184136033],
+    [41.002663591970276, 29.07182584047572]
+  ]);
+  List<LatLng> get getCoordinates => decodePolyline(coords).unpackPolyline();
 }
